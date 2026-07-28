a7abe310bede48995830a8a35e087bf38b020ec9
Aleksey-Yz <teehokot@gmail.com>
2026-07-28 13:56:05 +0300

    Практическая работа 6.6

---
+++ Source/Arkanoid/Public/Bonuses/BonusTimeSlow.h
@@ -0,0 +1,17 @@
+// Alexey Tishaninov. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Bonuses/BonusParent.h"
+#include "BonusTimeSlow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKANOID_API ABonusTimeSlow : public ABonusParent
+{
+	GENERATED_BODY()
+	
+};
